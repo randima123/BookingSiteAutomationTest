0985b7aba5e7d4db0a5e2741a0eed98d8953bf46
randima <randima.senanayake@novigi.com.au>
2020-09-12 15:49:16 +0530

    commenting for test.

--- SeleniumTesting/src/test/java/Tests/CalendarTest.java
+++ SeleniumTesting/src/test/java/Tests/CalendarTest.java
@@ -8,31 +8,29 @@ import org.testng.annotations.Test;
 public class CalendarTest extends BaseTest {
 
 
-    @Test(priority = 1, dataProviderClass = CommonDataProvider.class, dataProvider = "excelData")
-    public void selectCheckInDateTest(String location, String ciYear, String ciMonth, String ciDate, String coYear, String coMonth, String coDate){
-        HomePage homePage = HomePage.open();
-        homePage.clickDateDiv();
-        String checkInYear=ciYear;
-        String checkInMonth=ciMonth;
-        String checkInDay=ciDate;
-        homePage.selectDate(checkInYear,checkInMonth,checkInDay);
-        Assert.assertTrue(true);
-
-    }
-
-    @Test(priority = 2, dataProviderClass = CommonDataProvider.class, dataProvider = "excelData")
-    public void selectCheckOutDateTest(String location, String ciYear, String ciMonth, String ciDate, String coYear, String coMonth, String coDate) throws InterruptedException {
-        HomePage homePage = HomePage.open();
-        homePage.clickDateDiv();
-        String checkOutYear=coYear;
-        String checkOutMonth=coMonth;
-        String checkOutDay=coDate;
-        homePage.selectDate(checkOutYear,checkOutMonth,checkOutDay);
-        Thread.sleep(50);
-        Assert.assertTrue(true);
-
-    }
-
-
+//    @Test(priority = 1, dataProviderClass = CommonDataProvider.class, dataProvider = "excelData")
+//    public void selectCheckInDateTest(String location, String ciYear, String ciMonth, String ciDate, String coYear, String coMonth, String coDate){
+//        HomePage homePage = HomePage.open();
+//        homePage.clickDateDiv();
+//        String checkInYear=ciYear;
+//        String checkInMonth=ciMonth;
+//        String checkInDay=ciDate;
+//        homePage.selectDate(checkInYear,checkInMonth,checkInDay);
+//        Assert.assertTrue(true);
+//
+//    }
+//
+//    @Test(priority = 2, dataProviderClass = CommonDataProvider.class, dataProvider = "excelData")
+//    public void selectCheckOutDateTest(String location, String ciYear, String ciMonth, String ciDate, String coYear, String coMonth, String coDate) throws InterruptedException {
+//        HomePage homePage = HomePage.open();
+//        homePage.clickDateDiv();
+//        String checkOutYear=coYear;
+//        String checkOutMonth=coMonth;
+//        String checkOutDay=coDate;
+//        homePage.selectDate(checkOutYear,checkOutMonth,checkOutDay);
+//        Thread.sleep(50);
+//        Assert.assertTrue(true);
+//
+//    }
 
 }
